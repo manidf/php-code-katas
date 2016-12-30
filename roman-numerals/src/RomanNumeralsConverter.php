7d6b9a71f527e43e825f75f29d5094695ece0bfa
manidf <mannuel.ferreira@gmail.com>
2016-12-30 14:11:17 +0200

    lookup table and tests complete

--- roman-numerals/src/RomanNumeralsConverter.php
+++ roman-numerals/src/RomanNumeralsConverter.php
@@ -2,12 +2,20 @@
 
 class RomanNumeralsConverter
 {
-	
-	// 	static variable
-	    protected static $lookup = [
-	        50 => 'L'
-	    ];
-	
+    // 	static variable
+	protected static $lookup = [
+        1000 => 'M',
+        500 => 'D',
+        100 => 'C',
+        50 => 'L',
+        20 => 'XX',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I',
+    ];
+
 	public function convert($number)
 	{
 		$solution = '';
@@ -20,35 +28,6 @@ class RomanNumeralsConverter
 				$number -= $limit;
 			}
 		}
-		
-		while ($number >= '10') {
-			$solution .= 'X';
-			$number -= 10;
-		}
-		
-		while ($number >= '5') {
-			$solution .= 'V';
-			$number -= 5;
-		}
-		
-		while ($number >= '4') {
-			$solution .= 'IV';
-			$number -= 4;
-		}
-		
-		while ($number >= '9') {
-			$solution .= 'IX';
-			$number -= 9;
-		}
-		
-		while ($number >= '1')
-		        {
-			$solution .= 'I';
-			$number -= 1;
-		}
-		
-		$solution .= str_repeat('I', $number);
-		
 		
 		return $solution;
 		
