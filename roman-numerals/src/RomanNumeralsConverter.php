9ec79d4fb77196f81e7779ec4f6b9572be22dd30
manidf <mannuel.ferreira@gmail.com>
2016-12-30 14:01:24 +0200

    added lookup table and foreach statement to loop over the table of values

--- roman-numerals/src/RomanNumeralsConverter.php
+++ roman-numerals/src/RomanNumeralsConverter.php
@@ -2,21 +2,56 @@
 
 class RomanNumeralsConverter
 {
-    public function convert($number)
-    {
-
-        $solution = '';
-
-        if ($number >= '5') {
-            $solution .= 'V';
-            $number -= 5;
-        }
-        
-        $solution .= str_repeat('I', $number);
-
-
-        return $solution;
-        
-    }
-    
+	
+	// 	static variable
+	    protected static $lookup = [
+	        50 => 'L'
+	    ];
+	
+	public function convert($number)
+	{
+		$solution = '';
+		
+        foreach (static::$lookup as $limit => $glyph)
+		{
+			while ($number >= $limit)
+			{
+				$solution .= $glyph;
+				$number -= $limit;
+			}
+		}
+		
+		while ($number >= '10') {
+			$solution .= 'X';
+			$number -= 10;
+		}
+		
+		while ($number >= '5') {
+			$solution .= 'V';
+			$number -= 5;
+		}
+		
+		while ($number >= '4') {
+			$solution .= 'IV';
+			$number -= 4;
+		}
+		
+		while ($number >= '9') {
+			$solution .= 'IX';
+			$number -= 9;
+		}
+		
+		while ($number >= '1')
+		        {
+			$solution .= 'I';
+			$number -= 1;
+		}
+		
+		$solution .= str_repeat('I', $number);
+		
+		
+		return $solution;
+		
+	}
+	
 }
